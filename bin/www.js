66a8deb5cc5f790b0c17ead1fe12f801150f16ab
kmkmathan <kmkmathan@gmail.com>
2016-01-10 14:14:17 +0530

    bin file for process env.port

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,5 @@
 var app = require('../app');
-app.set('port',process.env.PORT||8080);
+app.set('port',process.env.PORT||8081);
 var server = app.listen(app.get('port'), function()
 {
 console.log('Express server listening on port ' + server.address().port);
